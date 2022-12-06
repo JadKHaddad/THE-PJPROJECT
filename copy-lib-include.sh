77eaef6a55cb0751b03133469ecc2472c7b81255
JadKHaddad <jadkhaddad@gmail.com>
2022-12-06 18:21:19 +0100

    moving to version 2.13

--- copy-lib-include.sh
+++ copy-lib-include.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 mkdir -p ./lib
-cp -R ./pjproject/pjsip/lib/*             ./lib
-cp -R ./pjproject/pjlib/lib/*             ./lib
-cp -R ./pjproject/pjlib-util/lib/*        ./lib
-cp -R ./pjproject/pjmedia/lib/*           ./lib
-cp -R ./pjproject/pjnath/lib/*            ./lib
-cp -R ./pjproject/third_party/lib/*       ./lib
-
 mkdir -p ./include
-cp -R ./pjproject/pjsip/include/*         ./include
-cp -R ./pjproject/pjlib/include/*         ./include
-cp -R ./pjproject/pjlib-util/include/*    ./include
-cp -R ./pjproject/pjmedia/include/*       ./include
-cp -R ./pjproject/pjnath/include/*        ./include
-
-#chmod u+x ./copy-lib-include.sh
+cp -R ./pjproject/pjsip/lib/* ./lib
+cp -R ./pjproject/pjlib/lib/* ./lib
+cp -R ./pjproject/pjlib-util/lib/* ./lib
+cp -R ./pjproject/pjmedia/lib/* ./lib
+cp -R ./pjproject/pjnath/lib/* ./lib
+cp -R ./pjproject/third_party/lib/* ./lib
+cp -R ./pjproject/pjsip/include/* ./include
+cp -R ./pjproject/pjlib/include/* ./include
+cp -R ./pjproject/pjlib-util/include/* ./include
+cp -R ./pjproject/pjmedia/include/* ./include
+cp -R ./pjproject/pjnath/include/* ./include
